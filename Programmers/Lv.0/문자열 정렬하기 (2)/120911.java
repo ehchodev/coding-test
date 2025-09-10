4f1db789bb083e6043be81ef4e6e2aa8e2f65a1d
ehchodev <ehcho1231@gmail.com>
2025-09-10 18:31:10 +0900

    Create 120911.java

---
+++ Programmers/Lv.0/문자열 정렬하기 (2)/120911.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        String[] arr = my_string.toLowerCase().split("");
+        Arrays.sort(arr);
+        
+        answer = String.join("", arr);
+        
+        return answer;
+    }
+}
