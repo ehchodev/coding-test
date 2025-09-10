e72b41b36af26ab574c3187a8b1574e252961778
ehchodev <ehcho1231@gmail.com>
2025-09-10 18:35:57 +0900

    Create 120924.java

---
+++ Programmers/Lv.0/다음에 올 숫자/120924.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        
+        int diff1 = common[1] - common[0];
+        int diff2 = common[2] - common[1];
+        
+        if (diff1 == diff2) {
+            answer = common[common.length-1] + diff1;
+        } else {
+            answer = common[common.length-1] * (common[1] / common[0]);
+        }
+        
+        return answer;
+    }
+}
