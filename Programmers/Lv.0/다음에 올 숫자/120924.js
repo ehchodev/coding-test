b8b0f8919c6d6371f9c6157e9518aa2112a2dd09
ehchodev <ehcho1231@gmail.com>
2025-09-10 18:36:19 +0900

    Create 120924.js

---
+++ Programmers/Lv.0/다음에 올 숫자/120924.js
@@ -0,0 +1,14 @@
+function solution(common) {
+    let answer = 0;
+    
+    const diff1 = common[1] - common[0];
+    const diff2 = common[2] - common[1];
+    
+    if (diff1 === diff2) {
+        answer = common[common.length-1] + diff1;
+    } else {
+        answer = common[common.length-1] * (common[1] / common[0]);
+    }
+    
+    return answer;
+}
