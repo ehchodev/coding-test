0a856ffc21ae72e5fa7c651d596ec853d7da7d4f
ehchodev <ehcho1231@gmail.com>
2025-09-10 18:36:42 +0900

    Create 120956.java

---
+++ Programmers/Lv.0/옹알이 (1)/120956.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        
+        for (int i=0; i<babbling.length; i++) {
+            babbling[i] = babbling[i].replace("aya", " ")
+                                     .replace("ye", " ")
+                                     .replace("woo", " ")
+                                     .replace("ma", " ");
+            
+            if (babbling[i].trim().equals("")) answer++;
+        }
+        
+        return answer;
+    }
+}
