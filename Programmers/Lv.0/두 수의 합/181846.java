18fa06c6085654bb0218c4004774740bcbfaa7d8
ehchodev <ehcho1231@gmail.com>
2025-09-13 11:18:52 +0900

    Create 181846.java

---
+++ Programmers/Lv.0/두 수의 합/181846.java
@@ -0,0 +1,8 @@
+import java.math.BigInteger;
+
+class Solution {
+    public String solution(String a, String b) {
+        String answer = String.valueOf(new BigInteger(a).add( new BigInteger(b) ));
+        return answer;
+    }
+}
