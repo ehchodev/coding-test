17c2075c9056c305a01107b2f875c2b0a2b43329
ehchodev <ehcho1231@gmail.com>
2025-09-13 11:19:52 +0900

    Create 181847.js

---
+++ Programmers/Lv.0/0 떼기/181847.js
@@ -0,0 +1,12 @@
+function solution(n_str) {
+    let answer = '';
+    
+    for (let i=0; i<n_str.length; i++) {
+        if (n_str.charAt(i) !== '0') {
+            answer = n_str.slice(i);
+            break;
+        }
+    }
+    
+    return answer;
+}
