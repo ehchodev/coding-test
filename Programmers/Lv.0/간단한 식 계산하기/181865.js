fd5a8da221fce786ea4a0522898323c7a78d1d4c
ehchodev <ehcho1231@gmail.com>
2025-09-18 18:51:46 +0900

    Create 181865.js

---
+++ Programmers/Lv.0/간단한 식 계산하기/181865.js
@@ -0,0 +1,10 @@
+function solution(binomial) {
+    let answer = 0;
+    
+    const [a, op, b] = binomial.split(' ');
+    if (op === '+') answer = Number(a) + Number(b);
+    else if (op === '-') answer = Number(a) - Number(b);
+    else answer = Number(a) * Number(b);
+    
+    return answer;
+}
