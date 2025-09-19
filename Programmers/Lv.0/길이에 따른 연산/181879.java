2c947a102d5ce1f0c23f54f23bc43cb9b89a7bf0
ehchodev <ehcho1231@gmail.com>
2025-09-19 19:33:45 +0900

    Create 181879.java

---
+++ Programmers/Lv.0/길이에 따른 연산/181879.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int length = num_list.length;
+        int answer = (length >= 11) ? 0 : 1;
+        
+        for (int num : num_list) {
+            if (length >= 11) {
+                answer += num;
+            } else {
+                answer *= num;
+            }
+        }
+        
+        return answer;
+    }
+}
