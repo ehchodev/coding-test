ae1184c80236512632b9736ea1010ba33a3e39c3
ehchodev <ehcho1231@gmail.com>
2025-09-20 15:51:43 +0900

    Create 181893.js

---
+++ Programmers/Lv.0/배열 조각하기/181893.js
@@ -0,0 +1,10 @@
+function solution(arr, query) {
+    const answer = arr;
+    
+    query.forEach((v, i) => {
+        if (i % 2 === 0) arr.splice(v + 1);
+        else arr.splice(0, v);
+    });
+    
+    return answer;
+}
