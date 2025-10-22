b7d1ac73ee4941db9577dbd91475ffc4eaded23c
ehchodev <ehcho1231@gmail.com>
2025-10-22 09:08:57 +0900

    Create 86051.java

---
+++ Programmers/Lv.1/없는 숫자 더하기/86051.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        
+        int[] arr = new int[10];
+        for (int n : numbers) {
+            arr[n]--;
+        }
+        
+        for (int i=0; i<arr.length; i++) {
+            if (arr[i] > -1) {
+                answer += i;
+            }
+        }
+        
+        return answer;
+    }
+}
