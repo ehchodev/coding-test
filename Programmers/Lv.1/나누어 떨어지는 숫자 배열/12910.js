1d47efb1d367a0203f575cf28c97aaef6c3ce57d
ehchodev <ehcho1231@gmail.com>
2025-10-22 09:10:04 +0900

    Create 12910.js

---
+++ Programmers/Lv.1/나누어 떨어지는 숫자 배열/12910.js
@@ -0,0 +1,6 @@
+function solution(arr, divisor) {
+    const answer = arr.filter(v => v % divisor === 0);
+    if (answer.length === 0) { answer.push(-1); }
+    answer.sort((a, b) => a - b);
+    return answer;
+}
