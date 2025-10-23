d2072c3bb02601154f1b41784dffccf494f8c90a
ehchodev <ehcho1231@gmail.com>
2025-10-23 14:47:51 +0900

    Create 12906.java

---
+++ Programmers/Lv.1/같은 숫자는 싫어/12906.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int[] arr) {
+        int[] answer = {};
+        
+        List<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+        
+        for (int i=1; i<arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                list.add(arr[i]);
+            }
+        }
+        
+        answer = new int[list.size()];
+        for (int j=0; j<list.size(); j++) {
+            answer[j] = list.get(j);
+        }
+
+        return answer;
+    }
+}
