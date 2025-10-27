858dda9628cae748b9c3d54bc2e86452f729eb24
ehchodev <ehcho1231@gmail.com>
2025-10-27 18:06:39 +0900

    Create 81301.java

---
+++ Programmers/Lv.1/숫자 문자열과 영단어/81301.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String s) {
+        String[] arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        int answer = 0;
+        
+        for (int i=0; i<arr.length; i++) {
+            s = s.replaceAll(arr[i], String.valueOf(i));
+        }
+        
+        answer = Integer.parseInt(s);
+        
+        return answer;
+    }
+}
