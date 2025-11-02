a68c4198ea308eecda759822de227c4b1b431141
ehchodev <ehcho1231@gmail.com>
2025-11-02 20:08:34 +0900

    Create 176963.java

---
+++ Programmers/Lv.1/추억 점수/176963.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        
+        for (int i=0; i<photo.length; i++) {
+            int score = 0;
+            
+            for (String p : photo[i]) {
+                for (int j=0; j<name.length; j++) {
+                    if (p.equals(name[j])) {
+                        score += yearning[j];
+                        break;
+                    }
+                }
+            }
+            
+            answer[i] = score;
+        }
+        
+        return answer;
+    }
+}
