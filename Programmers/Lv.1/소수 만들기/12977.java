689373385cceb632fe65fffc13f61896a020f0b8
ehchodev <ehcho1231@gmail.com>
2025-11-19 22:54:57 +0900

    Create 12977.java

---
+++ Programmers/Lv.1/소수 만들기/12977.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        for (int a = 0; a < nums.length - 2; a++) {
+            for (int b = a + 1; b < nums.length - 1; b++) {
+                for (int c = b + 1; c < nums.length; c++) {
+                    int sum = nums[a] + nums[b] + nums[c];
+                    boolean isPrime = true;
+                    for (int n = 2; n*n <= sum; n++) {
+                        if (sum % n == 0) {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                    
+                    if (isPrime) {
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
