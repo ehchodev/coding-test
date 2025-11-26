1d72f570b07fdacbceea095d93b7bd81b2015753
ehchodev <ehcho1231@gmail.com>
2025-11-26 21:45:28 +0900

    Create 42576.java

---
+++ Programmers/Lv.1/완주하지 못한 선수/42576.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
+        }
+        
+        for (String c : completion) {
+            if (map.get(c) == 1) {
+                map.remove(c);
+            } else {
+                map.put(c, map.get(c) - 1);
+            }
+        }
+        
+        answer = map.keySet().iterator().next();
+        
+        return answer;
+    }
+}
