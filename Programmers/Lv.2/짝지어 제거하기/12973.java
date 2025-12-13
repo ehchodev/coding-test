93cf051f11c041e98739ef1c8cdb9259d5dd7e6f
ehchodev <ehcho1231@gmail.com>
2025-12-13 22:37:27 +0900

    Create 12973.java

---
+++ Programmers/Lv.2/짝지어 제거하기/12973.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        
+        answer = (stack.isEmpty()) ? 1 : 0;
+
+        return answer;
+    }
+}
