375f2094a7d55c3c4b468aa57a13bf8a33dd87f3
Eunho Cho <ehcho1231@gmail.com>
2025-12-26 08:12:27 +0900

    Create 14681.java

---
+++ Baekjoon/브론즈/브론즈 V/사분면 고르기/14681.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+        
+        int quadrant = 0;
+        if (x > 0 && y > 0) {
+            quadrant = 1;
+        } else if (x < 0 && y > 0) {
+            quadrant = 2;
+        } else if (x < 0 && y < 0) {
+            quadrant = 3;
+        } else {
+            quadrant = 4;
+        }
+        
+        System.out.println(quadrant);
+    }
+}
