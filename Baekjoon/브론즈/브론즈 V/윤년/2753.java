065aafb0e25e6f3645a25e3bcf2546acbd5926c9
Eunho Cho <ehcho1231@gmail.com>
2025-12-26 08:11:46 +0900

    Create 2753.java

---
+++ Baekjoon/브론즈/브론즈 V/윤년/2753.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int year = Integer.parseInt(br.readLine());
+        
+        System.out.println(((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) ? 1 : 0);
+    }
+}
