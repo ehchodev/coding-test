3941bba5eaba1f2da45e2d02bed6e43428d0c794
Eunho Cho <ehcho1231@gmail.com>
2025-12-30 18:38:59 +0900

    Create 42578.java

---
+++ Programmers/Lv.2/의상/42578.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        
+        Map<String, Integer> map = new HashMap<>();
+        for (String[] c : clothes) {
+            map.put(c[1], map.getOrDefault(c[1], 0) + 1);
+        }
+        
+        for (String key : map.keySet()) {
+            answer *= map.get(key) + 1;
+        }
+        
+        answer--;
+        
+        return answer;
+    }
+}
