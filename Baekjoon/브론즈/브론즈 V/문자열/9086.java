f343b15b283e0bc0a56d47d2a8f8a65a1c7bc7de
Eunho Cho <ehcho1231@gmail.com>
2026-01-07 08:22:43 +0900

    Create 9086.java

---
+++ Baekjoon/브론즈/브론즈 V/문자열/9086.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < T; i++) {
+            String s = br.readLine();
+            System.out.println("" + s.charAt(0) + s.charAt(s.length() - 1));
+        }
+    }
+}
