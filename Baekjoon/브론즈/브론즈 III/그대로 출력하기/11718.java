03108d338e9d35281a06706cb0152e98bfc7db3c
Eunho Cho <ehcho1231@gmail.com>
2026-01-08 08:14:17 +0900

    Create 11718.java

---
+++ Baekjoon/브론즈/브론즈 III/그대로 출력하기/11718.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String S;
+        while ((S = br.readLine()) != null) {
+            System.out.println(S);
+        }
+    }
+}
