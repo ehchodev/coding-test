c4c0c2cb50ebbe2aec801b29d8f9958726891e9e
Eunho Cho <ehcho1231@gmail.com>
2026-01-15 08:27:48 +0900

    Create 2869.java

---
+++ Baekjoon/브론즈/브론즈 I/달팽이는 올라가고 싶다/2869.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+        
+        System.out.print((int) Math.ceil((double) (V - A) / (A - B)) + 1);
+    }
+}
