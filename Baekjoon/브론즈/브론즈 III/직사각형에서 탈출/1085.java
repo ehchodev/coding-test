a7fa445bf3a83dcba53a080c0edf8b010ebdeb27
Eunho Cho <ehcho1231@gmail.com>
2026-01-19 08:15:18 +0900

    Create 1085.java

---
+++ Baekjoon/브론즈/브론즈 III/직사각형에서 탈출/1085.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+        
+        System.out.print(Math.min(Math.min(Math.abs(w - x), x), Math.min(Math.abs(h - y), y)));
+    }
+}
