c025cfd84af3357dd5e4fb0f45f051f2f6578b5f
Eunho Cho <ehcho1231@gmail.com>
2026-01-19 08:17:11 +0900

    Create 9063.java

---
+++ Baekjoon/브론즈/브론즈 III/대지/9063.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st;
+        int x1 = 10000, x2 = -10000, y1 = 10000, y2 = -10000;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            
+            if (x < x1) x1 = x;
+            if (x > x2) x2 = x;
+            if (y < y1) y1 = y;
+            if (y > y2) y2 = y;
+        }
+        
+        System.out.print((x2 - x1) * (y2 - y1));
+    }
+}
