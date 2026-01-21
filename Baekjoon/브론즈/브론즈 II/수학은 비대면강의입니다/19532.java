10d41b6c8329a4e1eea78a41de2711e2f153405a
Eunho Cho <ehcho1231@gmail.com>
2026-01-22 08:24:21 +0900

    Create 19532.java

---
+++ Baekjoon/브론즈/브론즈 II/수학은 비대면강의입니다/19532.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+        
+        // 가감법
+        int x = (c * e - b * f) / (a * e - b * d);
+        int y = (c * d - a * f) / (b * d - a * e);
+        System.out.print(x + " " + y);
+    }
+}
