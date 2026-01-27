e36f146fadde66448e98af1bc2195b7c7b577d6b
Eunho Cho <ehcho1231@gmail.com>
2026-01-28 08:26:25 +0900

    Create 11651.java

---
+++ Baekjoon/실버/실버 V/좌표 정렬하기 2/11651.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        int[][] arr = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(arr, (a, b) -> {
+            if (a[1] == b[1]) {
+                return a[0] - b[0];
+            } else {
+                return a[1] - b[1];
+            }
+        });
+        
+        StringBuilder sb = new StringBuilder();
+        for (int[] xy : arr) {
+            sb.append(xy[0]).append(" ").append(xy[1]).append("\n");
+        }
+        
+        System.out.print(sb);
+    }
+}
