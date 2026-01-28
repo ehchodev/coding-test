c242ec17d06a289de5c1998a634410ab2588e613
Eunho Cho <ehcho1231@gmail.com>
2026-01-29 08:27:08 +0900

    Create 11478.java

---
+++ Baekjoon/실버/실버 III/서로 다른 부분 문자열의 개수/11478.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+        
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < S.length(); i++) {
+            for (int j = i + 1; j <= S.length(); j++) {
+                set.add(S.substring(i, j));
+            }
+        }
+        
+        System.out.print(set.size());
+    }
+}
