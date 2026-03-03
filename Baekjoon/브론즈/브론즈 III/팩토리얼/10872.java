d7a4982c425ece76e757f7b59d43f757e2f2c93e
ehchodev <ehcho1231@gmail.com>
2026-03-03 14:54:58 +0900

    Create 10872.java

---
+++ Baekjoon/브론즈/브론즈 III/팩토리얼/10872.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.print(factorial(N));
+    }
+    
+    public static int factorial(int n) {
+        if (n <= 1) return 1;
+        return n * factorial(n - 1);
+    }
+}
