b0bd4696b4e06c9cca304006208fa208334ad257
ehchodev <ehcho1231@gmail.com>
2026-03-10 09:33:48 +0900

    Create 10870.java

---
+++ Baekjoon/브론즈/브론즈 II/피보나치 수 5/10870.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.print(fibonacci(n));
+    }
+    
+    public static int fibonacci(int num) {
+        if (num == 0) return 0;
+        if (num == 1) return 1;
+        return fibonacci(num - 1) + fibonacci(num - 2);
+    }
+}
