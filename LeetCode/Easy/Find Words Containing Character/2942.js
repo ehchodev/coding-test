2f57959f4e23368a62e034f8b5a0e3af16dfbf45
ehchodev <ehcho1231@gmail.com>
2026-04-10 09:00:47 +0900

    Create 2942.js

---
+++ LeetCode/Easy/Find Words Containing Character/2942.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string[]} words
+ * @param {character} x
+ * @return {number[]}
+ */
+var findWordsContaining = function(words, x) {
+    return words.flatMap((v, i) => v.includes(x) ? i : []);
+};
