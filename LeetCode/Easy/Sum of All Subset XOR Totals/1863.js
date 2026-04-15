5460f19198c07cc2128ceb1be55a96e523cd5eff
ehchodev <ehcho1231@gmail.com>
2026-04-15 10:51:36 +0900

    Create 1863.js

---
+++ LeetCode/Easy/Sum of All Subset XOR Totals/1863.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var subsetXORSum = function(nums) {
+    const dfs = (i, sum) => {
+        if (i === nums.length) return sum;
+        return dfs(i + 1, sum ^ nums[i]) + dfs(i + 1, sum);
+    };
+    return dfs(0, 0);
+};
