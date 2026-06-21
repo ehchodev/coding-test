92b921ad1bfd4a3095661fb1754262ccae0b97e1
Eunho Cho <ehcho1231@gmail.com>
2026-06-22 08:16:12 +0900

    Update 2824.js

--- LeetCode/Easy/Count Pairs Whose Sum is Less than Target/2824.js
+++ LeetCode/Easy/Count Pairs Whose Sum is Less than Target/2824.js
@@ -7,11 +7,11 @@ var countPairs = function(nums, target) {
     let count = 0;
     const n = nums.length;
 
-    nums.sort((a, b) => b - a);
+    nums.sort((a, b) => a - b);
 
     let left = 0, right = n - 1;
     while (left < right) {
-        if (nums[i] + nums[j] < target) {
+        if (nums[left] + nums[right] < target) {
             count += (right - left);
             left++;
         } else {
