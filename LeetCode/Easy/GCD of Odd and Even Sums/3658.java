8c9faa9af051d154efd820bdcf6f30eb34b30e61
Eunho Cho <ehcho1231@gmail.com>
2026-07-28 08:33:23 +0900

    Create 3658.java

---
+++ LeetCode/Easy/GCD of Odd and Even Sums/3658.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        // 첫 n개 홀수의 합 : sumOdd = n^2
+        // 첫 n개 짝수의 합 : sumEven = n * (n + 1)
+        // 두 수의 최소공배수 : GCD = n
+        return n;
+    }
+}
