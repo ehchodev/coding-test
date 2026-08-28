b3608a728e7e443d888fee66f786a70c30329f8e
Eunho Cho <ehcho1231@gmail.com>
2026-08-28 09:17:59 +0900

    Create 3264.java

---
+++ LeetCode/Easy/Final Array State After K Multiplication Operations I/3264.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+        for (int i = 0; i < k; i++) {
+            int minIdx = 0;
+
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] < nums[minIdx]) {
+                    minIdx = j;
+                }
+            }
+
+            nums[minIdx] *= multiplier;
+        }
+
+        return nums;
+    }
+}
